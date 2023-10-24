475f341b00f770aef398cf7f2d2058fa87902266
Anton1oC4astr0 <143725409+Anton1oC4astr0@users.noreply.github.com>
2023-10-24 09:01:24 -0600

    Add files via upload

---
+++ Practica1U4.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.pkg1u4;
+import java.util.Scanner;
+/**
+ *
+ * @author labcca
+ */
+public class Practica1U4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double [] elementos = {20.5,40.6,78.4,94.7,55.3,22.6,84.3};
+        Scanner scanner = new Scanner (System.in);
+       
+        int opcion;
+        
+            System.out.println("Elija la opcion deseada");
+            System.out.println("1. Suma");
+            System.out.println("2. Multiplicacion");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    double suma = 0;
+                    for (int i = 0; i < elementos.length; i++) {
+                        suma = suma + elementos[i];
+                    }
+                        System.out.println("Resultado: " + suma);
+                        
+                        break;
+                        
+                case 2: 
+                    double multiplicacion = 1;
+                    for (int i = 0; i < elementos.length; i++) {
+                        multiplicacion = multiplicacion*elementos[i];
+                    }
+                    System.out.println("Resultado: " + multiplicacion);
+                    
+                    break;
+                default: 
+                    System.out.println("opcion no valida");
+
+                    }
+                
+            }
+        }
+    
+    
+
